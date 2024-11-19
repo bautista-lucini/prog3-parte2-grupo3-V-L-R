71944a86247c5597f099d6e38f2c228e074b450a
Victoria Valencia <mvalencia@udesa.edu.ar>
2024-11-19 11:08:48 -0300

    paquetes de stack y tab navigation, y cree la app en firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,7 +2,12 @@ import app from 'firebase/app';
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    
+    apiKey: "AIzaSyBIVYLHxFUWOsJ8WWOYGT51JQAAugFG27w",
+  authDomain: "pi-lrv-grupo3.firebaseapp.com",
+  projectId: "pi-lrv-grupo3",
+  storageBucket: "pi-lrv-grupo3.firebasestorage.app",
+  messagingSenderId: "748975476708",
+  appId: "1:748975476708:web:52ae6db560651f8c7501f1"
 }
 
 app.initializeApp(firebaseConfig);
